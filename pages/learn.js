5007ed506d61dafdd5a4c2996b35e9ed0059418a
faruqmaulana <faruqchodot21@gmail.com>
2022-03-03 14:11:20 +0700

    belajar membuat rest api

--- pages/learn.js
+++ pages/learn.js
@@ -3,7 +3,7 @@ import Beranda, { Contents } from "./Komponen";
 
 export default function learn() {
   return (
-    <div class="flex flex-col w-full min-h-screen">
+    <div className="flex flex-col w-full min-h-screen">
       <Navbar></Navbar>
       <Beranda></Beranda>
       <Contents></Contents>
