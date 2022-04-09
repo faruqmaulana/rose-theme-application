578635109ee5c9eded3378464fb34d8151c8c2ad
faruqmaulana <faruqchodot21@gmail.com>
2022-04-09 18:50:59 +0700

    update all files

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,51 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const axios = require("axios");
-const cheerio = require("cheerio");
-const Cors = require("cors");
 
-const url = "https://www.tribunnews.com/index-news";
-const cors = Cors({
-  methods: ["POST"],
-});
-
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
-    });
-  });
-}
-
-export default async function handler(req, res) {
-  await runMiddleware(req, res, cors);
-  let result = axios.get(url).then((res) => {
-    const html = res.data;
-    const $ = cheerio.load(html);
-
-    let list = $("select#sectionpil > option");
-    let index = [];
-
-    list.each(function (v, i) {
-      let link = url + "/" + $(this).attr("value");
-      let title = $(this).text();
-
-      index.push({
-        title,
-        link,
-      });
-    });
-    return {
-      message: "succes",
-      result: {
-        length: index.length,
-        data: index,
-      },
-    };
-  });
-
-  return res.json(await result);
+export default function handler(req, res) {
+  res.status(200).json({ name: "John Doe" });
 }
